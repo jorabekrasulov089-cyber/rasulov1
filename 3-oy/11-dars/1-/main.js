c67c14bde87a3260ce75b56f591774dbf1af7b8e
jorabek <jorabekrasulov089@gmail.com>
2026-04-16 11:57:19 +0300

    loyha toliq tayyor boldi

---
+++ 3-oy/11-dars/1-/main.js
@@ -0,0 +1,149 @@
+
+
+
+
+
+
+// let a = 5;
+
+// switch (a) {
+
+//   case 1:
+//     console.log("Yomon");
+//     break;
+//   case 2:
+//     console.log("Qoniqarsiz");
+//     break;
+//   case 3:
+//     console.log("Qoniqarli")
+//     break;
+//   case 4:
+//     console.log("Yaxshi");
+//     break;
+//   case 5:
+//   console.log("A'lo");
+//   break;
+//   default:
+//     console.log("1 dan 5 gacha bolgan son kiriting")
+// }
+
+// let oy = 12;
+
+// switch (oy) {
+//   case 12:
+//   case 1:
+//   case 2:
+//     console.log("Qish");
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("Bahor");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log("Yoz");
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log("Kuz");
+//     break;
+//   default:
+//     console.log("Noto'g'ri oy");
+// }
+
+// let yil = 2024;
+
+// if ((yil % 4 === 0 && yil % 100 !== 0) || yil % 400 === 0) {
+//   console.log("Kabisa yili");
+// } else {
+//   console.log("Oddiy yil");
+// }
+
+// let a = 10;
+// let b = 20;
+
+// let max = (a > b) ? a : b;
+// console.log("Katta son:", max);
+
+// let son = 7;
+
+// console.log((son % 2 === 0) ? "Juft" : "Toq");
+
+// let summa = 120000;
+// let kupon = true;
+
+// if (summa > 100000 && kupon) {
+//   console.log("20% chegirma");
+// } else if (summa > 100000) {
+//   console.log("5% chegirma");
+// } else {
+//   console.log("Chegirma yo'q");
+// }
+
+// let kun = 6;
+
+// switch (kun) {
+//   case 1:
+//     console.log("Dushanba");
+//     break;
+//   case 2:
+//     console.log("Seshanba");
+//     break;
+//   case 3:
+//     console.log("Chorshanba");
+//     break;
+//   case 4:
+//     console.log("Payshanba");
+//     break;
+//   case 5:
+//     console.log("Juma");
+//     break;
+//   case 6:
+//     console.log("Shanba - Dam olish kuni");
+//     break;
+//   case 7:
+//     console.log("Yakshanba - Dam olish kuni");
+//     break;
+//   default:
+//     console.log("Noto'g'ri kun");
+// }
+
+// let yosh = 16;
+// let otaOna = true;
+
+// if (yosh >= 18) {
+//   console.log("Ruxsat berildi");
+// } else if (yosh < 18 && otaOna) {
+//   console.log("Faqat ota-ona bilan ruxsat");
+// } else {
+//   console.log("Ruxsat berilmadi");
+// }
+
+// let son2 = -5;
+
+// if (son2 > 0) {
+//   console.log("Musbat");
+// } else if (son2 < 0) {
+//   console.log("Manfiy");
+// } else {
+//   console.log("Nol");
+// }
+
+// let qiymat = true;
+
+// switch (typeof qiymat) {
+//   case "number":
+//     console.log("Son kiritdingiz");
+//     break;
+//   case "string":
+//     console.log("Matn kiritdingiz");
+//     break;
+//   case "boolean":
+//     console.log("Mantiqiy qiymat");
+//     break;
+//   default:
+//     console.log("Boshqa tur");
+// }
